1e8787c0ce902973ca1303cd2e56b7183c886220
Len Chan <lenchan139@tto.moe>
2024-09-19 23:29:13 +0800

    v

---
+++ generate-hd2_vo_json.ts
@@ -0,0 +1,24 @@
+
+import * as fs from 'fs';
+function main(){
+    const urlPrefix = 'https://github.com/lenchan139/hd2_voice_bot/raw/refs/heads/master/'
+    const folderName = 'Helldivers 2 clips'
+const filelist = fs.readdirSync('./'+folderName);
+const json :any[]= []
+for(const f of filelist){
+const fname = f.split('.')[0]
+// console.log('f',fname)
+json.push({
+    type: 'voice',
+    id: `hd2vo_${f}`,
+    title:  fname,
+    caption: fname,
+    voice_url: `https://github.com/lenchan139/hd2_voice_bot/raw/refs/heads/master/${encodeURIComponent(fname)}/${encodeURIComponent(f)}`,
+ 
+})
+}
+console.log(json)
+fs.writeFileSync('./hd2_vo.json',JSON.stringify(json,null,2))
+// console.log(filelist)
+}
+main();
